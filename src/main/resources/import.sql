3836276d360f43283b57cfdf65fb5cf7457def00
abinasureskumar <abina30.08.2003@gmail.com>
2022-11-23 11:29:07 +0100

    created sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+-- DROP DATABASE dbUsers;
+-- CREATE DATABASE  dbUsers;
+-- USE dbUsers;
+
+-- create table users (
+--     id          serial,
+--     vorname     varchar(255) not null,
+--     nachname    varchar(255) not null,
+--     email       varchar(384) not null unique,
+--     passwort    varchar(255) not null,
+    
+--     primary key(id),
+-- );
